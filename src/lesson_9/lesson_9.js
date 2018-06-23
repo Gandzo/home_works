9ff30046d84a196a792a9a744ebde5de95d9e99b
Anna <gandzolee@gmail.com>
2018-06-23 22:46:57 +0300

    commit

--- src/lesson_9/lesson_9.js
+++ src/lesson_9/lesson_9.js
@@ -1,10 +1,34 @@
 import './lesson_9.scss';
 
-const someBtn = document.querySelector('button');
-console.log(someBtn);
-function greet() {
-    const name = prompt ('Hello world');
-    alert(`Hello, ${name} !`);
-}
+function notificationBar() {
+    const notificationBarElement = document.querySelector('.notification-bar');
+    const control = document.querySelector('.notification-bar__btn');
 
-someBtn.addEventListener('click', greet);
+
+    console.log(control);
+    console.log(notificationBarElement);
+    function toggleMessage() {
+        notificationBarElement.classList.toggle('notification-bar_active');
+    }
+    control.addEventListener('click' , toggleMessage );
+}
+notificationBar();
+
+// const someBtn = document.querySelector('button');
+// const btn = document.querySelector('.btn-hello');
+// console.dir(someBtn);
+//
+// function greet() {
+//     const name = prompt ('Hello world');
+// someBtn.classList.add('btn_touched')
+//     // someBtn.style.background = 'red';
+//     alert(`Hello, ${name} !`);
+// }
+// someBtn.addEventListener('click', greet);
+//
+// function hello() {
+//     alert('Hello');
+// }
+// btn.addEventListener('click', hello);
+//
+
